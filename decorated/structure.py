1ddef845b8595dd54f73137074fc6b0f325ec62d
Collin Dutter <collin@griptape.ai>
2025-02-06 09:22:53 -0800

    Use new version

--- decorated/structure.py
+++ decorated/structure.py
@@ -1,13 +1,5 @@
 from griptape.structures import Agent
-from griptape.utils.griptape_cloud_utils import GriptapeCloudContext
+from griptape.utils import GriptapeCloud
 
-
-@GriptapeCloudContext()
-def run(args: str) -> None:
-    Agent().run(args)
-
-
-with GriptapeCloudContext():
-    print("FOOOOOOOO")
-
-run("Write a poem")
+with GriptapeCloud():
+    Agent().run("What is 2 + 2")
