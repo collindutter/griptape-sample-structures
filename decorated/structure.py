619d99f007f0fc7999b2274dee54c08090da52b5
Collin Dutter <collin@griptape.ai>
2025-02-10 15:42:22 -0800

    Fix util

--- decorated/structure.py
+++ decorated/structure.py
@@ -1,5 +1,5 @@
 from griptape.structures import Agent
-from griptape.utils import GriptapeCloud
+from griptape.utils import GriptapeCloudStructure
 
-with GriptapeCloud(observe=True):
+with GriptapeCloudStructure(observe=True):
     Agent(stream=True).run("What is 2 + 2")
