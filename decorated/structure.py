9d58763963914468fd2ec6a5332b216bff89ea4e
Collin Dutter <collin@griptape.ai>
2025-02-11 11:05:47 -0800

    Use args

--- decorated/structure.py
+++ decorated/structure.py
@@ -1,5 +1,6 @@
+import sys
 from griptape.structures import Agent
 from griptape.utils import GriptapeCloudStructure
 
 with GriptapeCloudStructure(observe=True):
-    Agent(stream=True).run("What is 2 + 2")
+    Agent(stream=True).run(*sys.argv[1:])
